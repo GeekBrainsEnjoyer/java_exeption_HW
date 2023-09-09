fd1662a733ad568cb56d444207b4c975e26dd70c
Александр Рахматулин <125355824+GeekBrainsEnjoyer@users.noreply.github.com>
2023-09-09 20:50:21 +0300

    Delete 3/untitled directory
    удаляю кринж

--- 3/untitled/src/main/java/MVP/View.java
+++
@@ -1,9 +0,0 @@
-package MVP;
-
-public interface View {
-    String getFIO();
-    String getBirthday();
-    String getPhone();
-    String getGender();
-
-}
--- 3/untitled/src/main/java/Program.java
+++
@@ -1,7 +0,0 @@
-import UI.App;
-
-public class Program {
-    public static void main(String[] args) {
-        App.Start();
-    }
-}
